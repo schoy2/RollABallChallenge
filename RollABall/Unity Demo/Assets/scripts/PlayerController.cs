de4129a573d4ad196ec02a0b2562f93652671bb3
gravesr1993 <gravesr1993@gmail.com>
2015-01-24 14:17:39 -0700

    Initial add of unity demo
    Unity Demo of Roll-A-Ball

---
+++ RollABall/Unity Demo/Assets/scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	private int count;
+
+
+	void Start()
+	{
+		count = 0;
+		SetCountText ();
+
+	}
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Pickup") 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText();
+		}
+		if (count >= 12) {
+			winText.text="YOU WIN!";		
+		}
+		//Destroy(other.gameObject);
+	}
+	void SetCountText()
+	{
+		countText.text = "count: "+count.ToString();
+	}
+}
