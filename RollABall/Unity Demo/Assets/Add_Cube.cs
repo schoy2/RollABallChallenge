29c1e1f9a9109b2747ce7f19c6632a05aaa2578d
schoy2 <samuelchoy2@gmail.com>
2015-01-24 18:29:06 -0700

    This is an updated version

---
+++ RollABall/Unity Demo/Assets/Add_Cube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Add_Cube : MonoBehaviour
+{
+	public GameObject brick;
+	public bool toggle;
+	void Start() {
+		}
+	public void AddObject()
+	{
+		if (toggle)
+		{
+			Instantiate(brick, new Vector3(0,0, 0), Quaternion.identity);
+			
+			}
+		}
+	//}
+}
